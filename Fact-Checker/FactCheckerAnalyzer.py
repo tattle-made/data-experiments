56241f070e55982105271f5ab79efbf446cb24fe
mlkorra <korrarahuldev@gmail.com>
2022-01-21 17:13:13 +0530

    Update script with all  basic statistics and visualizations

--- Fact-Checker/FactCheckerAnalyzer.py
+++ Fact-Checker/FactCheckerAnalyzer.py
@@ -7,6 +7,8 @@ import calendar
 import pandas as pd
 import json
 import plotly.express as px
+import date_converter
+import plotly.graph_objects as go
 
 DB_NAME = os.environ.get("DB_NAME")
 DB_COLLECTION = os.environ.get("DB_COLLECTION")
@@ -38,8 +40,9 @@ def get_lastweek_data(coll,today=get_today_date()):
         start_of_week = date_obj - timedelta(days=date_obj.weekday()) - timedelta(days=7) # Last Monday
         end_of_week = start_of_week + timedelta(days=6)  # Sunday
         print(start_of_week)
+        #print(type(start_of_week))
         print(end_of_week)
-        
+    
     else:
         
         date_obj = datetime.strptime(today, '%Y-%m-%d')
@@ -54,24 +57,50 @@ def get_lastweek_data(coll,today=get_today_date()):
     if docs_df.empty:
         return docs_df
 
+    docs_df.drop_duplicates(subset = "postURL", inplace = True)
     docs_df = docs_df.explode('docs').reset_index(drop=True)
     docs_df['media_type'] = docs_df['docs'].apply(lambda x: x.get('mediaType'))
+    print(docs_df['media_type'].unique())
+    value_counts = docs_df['media_type'].value_counts()
+    print(value_counts)
+    docs_df['factcheck_site'] = docs_df['domain'].apply(lambda x : x.split('.')[0])
+
     print(docs_df.shape)
 
-    return docs_df
+    start_of_week = date_converter.datetime_to_string(start_of_week, '%Y-%m-%d')
+    end_of_week = date_converter.datetime_to_string(end_of_week,'%Y-%m-%d')
+
+    return docs_df,start_of_week,end_of_week
+
+def get_week_num(input_date):
+
+    input_date = date_converter.string_to_datetime(input_date, '%Y-%m-%d', '%Y-%m-%d')
+    _, week_num, _ = input_date.isocalendar()
+
+    return week_num
 
 def num_articles(docs):
     
     num = len(pd.unique(docs['postURL']))
     return num
 
-def day_wise_count(docs):
+def day_wise_count(docs,week):
     
-    df    =  docs.groupby(['date_updated_UTC']).size().reset_index(name='counts')
-    df.columns = ['date','count']
-    fig   =  px.bar(df, x='date', y='count')
-    filename =  day_wise_count.__name__ + ".html"
+    #df    =  docs.groupby(['date_updated_UTC']).size().reset_index(name='counts')
+    #df.columns = ['date','count']
+    #fig   =  px.bar(df, x='date', y='count')
+    docs_dup = docs.drop_duplicates(subset = "postURL")
 
+    df = docs_dup.groupby(['date_updated_UTC']).size().reset_index(name='counts')
+    #import plotly.express as px
+    df.columns  = ['date','count']
+    fig = px.line(df, x="date", y="count", markers=True, template="simple_white")
+    fig.update_xaxes(showgrid=False)
+    fig.update_yaxes(showgrid=True)
+    fig.update_traces(line=dict(color="#9467bd",width=1.5))
+    fig.update_layout(title="Day Wise Distribution of Articles",xaxis_title="date",yaxis_title="number of articles")
+    
+    filename = week + "_" + day_wise_count.__name__ + ".html"
     fig.write_html(filename)
 
     return filename
@@ -80,33 +109,105 @@ def count_domains(docs):
     
     return len(docs['domain'].unique())
 
-def domain_wise_count(docs):
+def domain_wise_count(docs,week):
     
-    docs['factcheck_site'] = docs['domain'].apply(lambda x : x.split('.')[0])
-    return docs.groupby(['factcheck_site']).size().reset_index(name='counts')
-
-def dist_media_type(docs):
+    # docs['factcheck_site'] = docs['domain'].apply(lambda x : x.split('.')[0])
+    # return docs.groupby(['factcheck_site']).size().reset_index(name='counts')
     
-    docs['media_type'] = docs['docs'].apply(lambda x: x.get('mediaType'))    
-    return docs['media_type'].value_counts()
-
-def count_media_wrt_day(docs):
+    docs_dup = docs.drop_duplicates(subset = "postURL")
+    #docs.drop_duplicates(subset = "postURL", inplace = True)
+    df = docs_dup.groupby(['factcheck_site']).size().reset_index(name='counts')
     
-    return docs.groupby('date_updated_UTC')['media_type'].value_counts()
+    x = df['factcheck_site']
+    y = df['counts']
 
-def count_media_wrt_domain(docs):
+    fig = go.Figure(data=[go.Bar(x=x,y=y,width=0.5)])
+    fig.update_traces(marker_color='#f08080', marker_line_color='#663399',marker_line_width=2.5, opacity=0.7)
+    fig.update_layout(title="Domain Wise Distribution of Articles",xaxis_title="factcheck_site",yaxis_title="number of articles")
+    fig.layout.plot_bgcolor = '#fff'
     
-    return docs.groupby('factcheck_site')['media_type'].value_counts()
+    filename = week + "_" + domain_wise_count.__name__ + ".html"
+    fig.write_html(filename)
 
+    return filename
 
-#returns dict
-def count_articles_wrt_socialmedia(docs):
+def dist_media_type(docs,week):
+    
+    value_counts = docs['media_type'].value_counts()
+    print(value_counts)
+    df = pd.DataFrame(value_counts)
+    df = df.reset_index()
+    df.columns = ['media_type', 'counts'] 
+    
+    x = df['media_type']
+    y = df['counts']
+
+    fig = go.Figure(data=[go.Bar(x=x,y=y,width=0.5)])
+    fig.update_traces(marker_color='#f08080', marker_line_color='#663399',marker_line_width=2.5, opacity=0.7)
+    fig.update_layout(title="Count of Media types across all articles",xaxis_title="media_type",yaxis_title="count")
+    fig.layout.plot_bgcolor = '#fff'
+    
+    filename = week + "_" + dist_media_type.__name__ + ".html"
+    fig.write_html(filename)
+
+    return filename
+
+def count_media_wrt_day(docs,week):
+    
+    docs.groupby('date_updated_UTC')['media_type'].value_counts()
+    df = docs.groupby( [ "date_updated_UTC", "media_type"] ).size().to_frame(name = 'count').reset_index()
+
+    fig = px.line(df, x="date_updated_UTC", y="count", color='media_type',markers=True, template="simple_white", color_discrete_sequence=px.colors.qualitative.Prism)
+    fig.update_xaxes(showgrid=False)
+    fig.update_layout(title="Daywise count of Media types",xaxis_title="date",yaxis_title="count")
+    
+    filename = week + "_" + count_media_wrt_day.__name__ + ".html"
+    fig.write_html(filename)
+
+    return filename
+
+def count_media_wrt_domain(docs,week):
+    
+    
+    docs.groupby('factcheck_site')['media_type'].value_counts()
+    df = docs.groupby( [ "factcheck_site", "media_type"] ).size().to_frame(name = 'count').reset_index()
+
+    fig = px.bar(df, x="factcheck_site", y="count", color="media_type",text_auto=True)
+    fig.update_xaxes(showgrid=False)
+    fig.update_yaxes(showgrid=False)
+    fig.update_traces(marker_line_color='#663399',marker_line_width=2, opacity=0.8)
+    fig.update_layout(title="Domain wise count of Media types",xaxis_title="factcheck_site",yaxis_title="count")
+    fig.layout.plot_bgcolor = '#fff'
+    
+    filename = week + "_" + count_media_wrt_domain.__name__ + ".html"
+    fig.write_html(filename)
+
+    return filename
+
+def count_articles_wrt_socialmedia(docs,week):
     
     a = len(pd.unique(docs[docs['media_type']=="tweet"]['postURL']))
     b = len(pd.unique(docs[docs['media_type']=="fbpost"]['postURL']))
     c = len(pd.unique(docs['postURL']))
     
-    return {"tweet" : round(a/c),"fbposts" : round(b/c)}
+    count_dict = {"tweet" : float(a/c)*100,"fbposts" : float(b/c)*100}
+    
+    df = pd.DataFrame.from_dict(count_dict,orient='index')
+    df = df.reset_index()
+    df.columns = ['social_media', 'counts'] 
+
+    x = df['social_media']
+    y = df['counts']
+
+    fig = go.Figure(data=[go.Bar(x=x,y=y,width=0.5)])
+    fig.update_traces(marker_color='#f08080', marker_line_color='#663399',marker_line_width=2.5, opacity=0.7)
+    fig.update_layout(title="Count of Social Media posts across all articles",xaxis_title="social media",yaxis_title="count")
+    fig.layout.plot_bgcolor = '#fff'
+    
+    filename = week + "_" + count_articles_wrt_socialmedia.__name__ + ".html"
+    fig.write_html(filename)
+
+    return filename  
 
 def avg_images_per_article(docs):
     
@@ -120,16 +221,31 @@ def get_authors_list(docs):
     
     return list(docs['author_name'].unique())
 
+def convert_data_to_string(date):
+    
+    str_date = date_converter.string_to_string(date, '%Y-%m-%d', '%d %B %Y')
+    str_date = str_date.replace(" ", "")
+    
+    return str_date
+
 if __name__ == "__main__":
 
     coll = initialize_mongo()
+    input_date = "2021-05-01"
+    data,start,end = get_lastweek_data(coll,input_date)
+
     
-    data = get_lastweek_data(coll,"2021-05-01")
-    #data = get_lastweek_data(coll)
-    
-    print(data)
+    print(f"input date : {input_date}")
+
+    """
+    Fetch data today
+    data = get_lastweek_data(coll)
+    """
 
     if not data.empty:
+
+        week = convert_data_to_string(start) + "-" + convert_data_to_string(end)
+        #docs_df.drop_duplicates(subset = "postURL", inplace = True)
 
         analysis_dict = {
             "status" : "Found Data",
@@ -137,9 +253,16 @@ if __name__ == "__main__":
             "num_sites" : count_domains(data), 
             "authors_list" : get_authors_list(data),  
             "avg_images_per_article" : avg_images_per_article(data),
-            "day_wise_count" : day_wise_count(data),
+            "count_wrt_day" : day_wise_count(data,week),
+            "count_wrt_domain" : domain_wise_count(data,week),
+            "count_wrt_mediatype" : dist_media_type(data,week),
+            "count_media_wrt_day" : count_media_wrt_day(data,week),
+            "count_media_wrt_domain" : count_media_wrt_domain(data,week),
+            "count_articles_wrt_socialmedia" : count_articles_wrt_socialmedia(data,week),
+            "week" : week,
+            "week_num" : get_week_num(start)    
         }
-
+    
     else:
 
         analysis_dict = {
@@ -149,4 +272,3 @@ if __name__ == "__main__":
     with open('analysis1.json','w') as json_file:
 
             json.dump(analysis_dict,json_file)
- 
