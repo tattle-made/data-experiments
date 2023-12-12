ae2a7f89824798e11496de3a5aec188fa3ba5a40
Aurora <5505558+duggalsu@users.noreply.github.com>
2023-12-12 18:41:24 +0530

    Add GPT4-Vision script - Added GPT4-Vision script - Updated requirements

---
+++ claims_extraction/src/gpt4_vision.py
@@ -0,0 +1,50 @@
+import base64
+import requests
+
+# TODO: OpenAI API Key
+api_key = "<api-key>"
+
+
+# Function to encode the image
+def encode_image(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode('utf-8')
+
+
+# TODO: Path to your image
+img_path = "<img path>"
+
+# Getting the base64 string
+base64_image = encode_image(img_path)
+
+headers = {
+  "Content-Type": "application/json",
+  "Authorization": f"Bearer {api_key}"
+}
+
+payload = {
+  "model": "gpt-4-vision-preview",
+  "messages": [
+    {
+      "role": "user",
+      "content": [
+        {
+          "type": "text",
+          "text": "What’s in this image?"
+        },
+        {
+          "type": "image_url",
+          "image_url": {
+            "url": f"data:image/jpeg;base64,{base64_image}",
+            "detail": "low"
+          }
+        }
+      ]
+    }
+  ],
+  "max_tokens": 300
+}
+
+response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+
+print(response.json())
